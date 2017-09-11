162fd53281123ce1a8cd34e14093f1825c771f5e
NarenKrishnaa <narenkrish@NarenKrishnaas-MacBook-Pro.local>
2017-09-11 08:24:05 +0530

    UI designs,Util files,MVC

---
+++ BuildingHub/StaticConstants.swift
@@ -0,0 +1,9 @@
+//
+//  StaticConstants.swift
+//  BuildingHub
+//
+//  Created by NarenKrishnaa on 10/09/17.
+//  Copyright © 2017 NarenKrishnaa. All rights reserved.
+//
+
+import Foundation
---
+++ BuildingHub/TapGesture.swift
@@ -0,0 +1,9 @@
+//
+//  TapGesture.swift
+//  BuildingHub
+//
+//  Created by NarenKrishnaa on 10/09/17.
+//  Copyright © 2017 NarenKrishnaa. All rights reserved.
+//
+
+import Foundation
